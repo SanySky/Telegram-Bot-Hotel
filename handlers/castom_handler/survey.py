ca46c62a57474001f406de8b99de0c8fdd4ad798
Alexandr Skitzky <skitzky@yandex.ru>
2023-09-16 22:58:05 +0300

    Добавление данных в __init__

--- handlers/castom_handler/survey.py
+++ handlers/castom_handler/survey.py
@@ -1,11 +1,14 @@
+from keyboards.reply.contact import request_contact
 from loader import bot
 from states.contact_information import UserInfoState
 from telebot.types import Message
+
 
 @bot.message_handler(commands=["survey"])
 def survey(message: Message) -> None:
     bot.set_state(message.from_user.id, UserInfoState.name, message.chat.id)
     bot.send_message(message.from_user.id, f'Привет, {message.from_user.username} введи свое имя')
+
 
 @bot.message_handler(state=UserInfoState.name)
 def get_name(message: Message) -> None:
@@ -39,6 +42,26 @@ def get_country(message: Message) -> None:
     with bot.retrieve_data(message.from_user.id, message.chat.id) as data:
         data['country'] = message.text
 
+
 @bot.message_handler(state=UserInfoState.city)
 def get_city(message: Message) -> None:
-    bot.send_message(message.from_user.id, f'Спасибо записал, теперь отправь свой номер, для этого нажми на кнопку.')
+    bot.send_message(message.from_user.id, f'Спасибо записал, теперь отправь свой номер, для этого нажми на кнопку.',
+                     reply_markup=request_contact())
+    bot.set_state(message.from_user.id, UserInfoState.number_telephone, message.chat.id)
+
+    with bot.retrieve_data(message.from_user.id, message.chat.id) as data:
+        data['city'] = message.text
+
+
+@bot.message_handler(content_types=['text', 'contact'], state=UserInfoState.number_telephone)
+def get_number_telephone(message: Message) -> None:
+    if message.content_type == 'contact':
+        with bot.retrieve_data(message.from_user.id, message.chat.id) as data:
+            data['number_telephone'] = message.contact.phone_number
+
+        text = f'Спасибо, что предоставили нам информацию, ваши данные\n ' \
+               f'Имя - {data["name"]}\n Возраст - {data["age"]}\n Страна - {data["country"]}\n ' \
+               f'Город - {data["city"]}\n Телефон - {data["number_telephone"]}'
+        bot.send_message(message.from_user.id, f'{text}')
+    else:
+        bot.send_message(message.from_user.id, f'Чтобы отправить информацию, нажми на кнопку')
--- main.py
+++ main.py
@@ -1,9 +1,9 @@
-from loader import bot
 from telebot.custom_filters import StateFilter
-import handlers  # noqa
+from loader import bot
+import handlers
 from utils.set_bot_commands import set_default_commands
 
 if __name__ == "__main__":
-    bot.add_custom_filters(StateFilter(bot))
+    bot.add_custom_filter(StateFilter(bot))
     set_default_commands(bot)
     bot.infinity_polling()
