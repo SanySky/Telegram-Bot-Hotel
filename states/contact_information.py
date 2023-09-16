e2c2d610eac29172be44fbbc5f802962e0e7d207
Alexandr Skitzky <skitzky@yandex.ru>
2023-09-16 23:06:41 +0300

    Изменение состояний

--- states/contact_information.py
+++ states/contact_information.py
@@ -3,10 +3,10 @@ from telebot.handler_backends import State, StatesGroup
 
 
 class UserInfoState(StatesGroup):
-    name = State
-    age = State
-    country = State
-    city = State
-    number_telephone = State
+    name = State()
+    age = State()
+    country = State()
+    city = State()
+    number_telephone = State()
 
 
